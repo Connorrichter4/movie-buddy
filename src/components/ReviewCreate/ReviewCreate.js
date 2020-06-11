857f405c8cfc46ffd94272dd06ba619fbdcb3589
Connorrichter4 <Connorrichter4@gmail.com>
2020-06-11 13:56:52 -0500

    finished create review

--- src/components/ReviewCreate/ReviewCreate.js
+++ src/components/ReviewCreate/ReviewCreate.js
@@ -1,14 +1,76 @@
-import React from 'react';
-import ReviewForm from '../ReviewForm/ReviewForm'
-import {APIURL} from '../../config';
+import React, { useState } from 'react';
+import ReviewForm from '../ReviewForm/ReviewForm';
+import { APIURL } from '../../config';
+import {Redirect} from 'react-router-dom'
 
 function ReviewCreate() {
-    return (
-        <>
-            <h1>Create Review</h1>
-            <ReviewForm />
-        </>
-    )
+	const initialState = {
+		title: '',
+		movie: '',
+		review_body: '',
+	};
+
+	const [review, setReview] = useState(initialState);
+	const [validate, setValidate] = useState(false);
+	const [success, setSuccess] = useState(false);
+	const [newReviewId, setNewReviewId] = useState('');
+	// const [errorExists, setErrorExists] = useState(false);
+
+	const validateData = () => {
+		for (const value in review) {
+			if (review[value].length === 0) {
+				setValidate(true);
+				return false;
+			}
+		}
+		return true;
+	};
+
+	const handleChange = (event) => {
+		event.persist();
+		setReview({ ...review, [event.target.name]: event.target.value });
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		if (validateData()) {
+			fetch(`${APIURL}/reviews/`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${localStorage.getItem('token')}`,
+				},
+				body: JSON.stringify({
+					title: review.title,
+					review_body: review.review_body,
+					movie: parseInt(review.movie),
+				}),
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					console.log(data);
+					setNewReviewId(data.id);
+					setSuccess(true);
+				})
+				.catch(console.error);
+		}
+	};
+
+	if (success) {
+		return <Redirect to={`/reviews/${newReviewId}`} />;
+	}
+
+	return (
+		<>
+			<h1>Create Review</h1>
+			<ReviewForm
+				handleChange={handleChange}
+				handleSubmit={handleSubmit}
+				validate={validate}
+			/>
+		</>
+	);
 }
 
 export default ReviewCreate;
--- src/components/ReviewForm/ReviewForm.js
+++ src/components/ReviewForm/ReviewForm.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './ReviewForm.css';
 import {APIURL} from '../../config';
 
-function ReviewForm() {
+function ReviewForm(props) {
 	const [movies, setMovies] = useState([]);
 
 	useEffect(() => {
@@ -10,37 +10,45 @@ function ReviewForm() {
 			.then((res) => res.json())
 			.then((data) => {
 				setMovies(data);
-				console.log(data);
 			})
 			.catch(console.error);
 	}, []);
 
 	return (
-		<form className='review-form'>
+		<form className='review-form' onSubmit={props.handleSubmit}>
 			<label htmlFor='title'>Title:</label>
 			<input
 				type='text'
 				placeholder='Add review title'
-				required
 				name='title'
+				onChange={props.handleChange}
 				className='input-area'
 			/>
 			<label htmlFor='body'>Body:</label>
 			<textarea
 				rows='12'
 				placeholder='Add review here'
-				required
-				name='body'
+				name='review_body'
+				onChange={props.handleChange}
 				className='input-area'
 			/>
-			<label htmlFor='body'>Movie:</label>
-			<select required name='movie' className='input-area' placeholder='Select a Movie'>
+			<label htmlFor='movie'>Movie:</label>
+			<select name='movie' className='input-area' onChange={props.handleChange}>
+				<option defaultValue>Select a movie</option>
 				{movies.map((movie) => (
 					<option key={movie.id} value={movie.id}>
 						{movie.title}
 					</option>
 				))}
 			</select>
+			{props.validate && (
+				<p className='review-error'>
+					All fields are required
+				</p>
+			)}
+			<button type='submit' className='submit-button'>
+				Submit
+			</button>
 		</form>
 	);
 }
