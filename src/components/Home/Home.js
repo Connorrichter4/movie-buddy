b1792d21c5f14553a7e58b1823c238d9b311eb26
Connorrichter4 <Connorrichter4@gmail.com>
2020-06-15 14:33:56 -0500

    updated home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+import { APIURL } from '../../config';
+
 import './Home.css';
 
 function Home() {
+	const [reviews, setReviews] = useState([]);
+	const [movies, setMovies] = useState([]);
+	const [error, setError] = useState(false);
+
+	useEffect(() => {
+		fetch(`${APIURL}/reviews/`)
+			.then((res) => res.json())
+			.then((data) => {
+				data.sort((a, b) => (a.updated_at - b.updated_at ? 1 : -1));
+				setReviews(data);
+				console.log(data);
+			})
+			.catch(() => {
+				setError(true);
+			});
+
+		fetch(`${APIURL}/movies/`)
+			.then((res) => res.json())
+			.then((data) => {
+				data.sort((a, b) => (a.created - b.created ? 1 : -1));
+				setMovies(data.slice(0,4));
+				console.log(data.slice(0,4));
+			})
+			.catch(() => {
+				setError(true);
+			});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	if (error) {
+		return (
+			<div className='error-retrieve'>
+				There was an error retrieving the info
+			</div>
+		);
+	}
+
+	if (!reviews || !movies) {
+		return <p>Loading ...</p>;
+	}
 	return (
 		<div>
 			<div className='home-image'>
@@ -9,6 +52,29 @@ function Home() {
 					<span>Post Reviews</span>
 					<span>Share Movies</span>
 				</div>
+			</div>
+			<div>
+				<h2>Recently Added Movies: </h2>
+				<div className='recently-added'>
+					{movies.map((movie) => (
+						<Link
+							to={`/movies/${movie.id}`}
+							key={movie.id}
+							className='movie-link'>
+							<img
+								className='recently-added-image'
+								src={movie.image_url}
+								alt={movie.title}
+							/>
+						</Link>
+					))}
+					<Link to='/movies' className='see-more-link'>
+						<p className='see-more-movies'>See More ...</p>
+					</Link>
+				</div>
+			</div>
+			<div>
+				<h2>Reviews: </h2>
 			</div>
 		</div>
 	);
